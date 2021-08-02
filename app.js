309d8a4a131179d1ebd77a1d810504efb62d231f
gpasha <g1ebikpasha@gmail.com>
2021-08-02 19:20:30 +0300

    connect mongo, model user, router register/ login

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const config = require('config')
+const mongoose = require('mongoose')
+
+const app = express()
+
+app.use('/api/auth', require('./router/auth.router'))
+
+const PORT = config.get('port') || 5000
+const MONGO_URI = config.get('mongoUri')
+
+async function start() {
+    try {
+        await mongoose.connect(MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+        app.listen(PORT, () => {
+            console.log(`The APP has been started on host: ${PORT}`)
+        })
+    } catch(e) {
+        console.log(e)
+        process.exit(1)
+    }
+}
+
+start()
---
+++ router/auth.router.js
@@ -0,0 +1,87 @@
+const {Router} = require('express')
+const router = Router()
+const User = require('../models/user')
+const bcrypt = require('bcryptjs')
+const {check, validationResult} = require('express-validator')
+
+router.post(
+    '/register',
+    [
+        check('email', 'Non-correct email!').isEmail(),
+        check('password', 'Min length of the password is 6 symbols').isLength({min: 6})
+    ],
+    async (res, req) => {
+        try {
+            const errors = validationResult(req)
+
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    errors: errors.array(),
+                    message: 'Non-correct regicter data'
+                })
+            }
+
+            const {email, password} = req.body
+            const condidate = await User.findOne({email})
+
+            if (condidate) {
+                return res.status(400).json({message: 'This user has already existed!'})
+            }
+
+            const hashedPassword = await bcrypt.hash(password, 12)
+            const user = new User({
+                email, password: hashedPassword
+            })
+            await user.save()
+            res.status(201).json({message: 'The user was successfully created!'})
+        } catch (e) {
+            console.log(e)
+            res.status(500).json({message: 'Something went wrong'})
+        }
+    }
+)
+
+router.post(
+    '/login',
+    [
+        check('email', 'Non-correct email!').normalizeEmail().isEmail(),
+        check('password', 'Min length of the password is 6 symbols').exists()
+    ],
+    async (res, req) => {
+        try {
+            const errors = validationResult(req)
+
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    errors: errors.array(),
+                    message: 'Non-correct regicter data'
+                })
+            }
+
+            const {email, password} = req.body
+            const user = await User.findOne({ email })
+
+            if (!user) {
+                return res.status(400).json({message: 'This user has not existed!'})
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password)
+
+            if (!isMatch) {
+                return res.status(400).json({message: 'There are some troubles of login and password'})
+            }
+
+            // const hashedPassword = await bcrypt.hash(password, 12)
+            // const user = new User({
+            //     email, password: hashedPassword
+            // })
+            // await user.save()
+            // res.status(201).json({message: 'The user was successfully created!'})
+        } catch (e) {
+            console.log(e)
+            res.status(500).json({message: 'Something went wrong'})
+        }
+    }
+)
+
+module.exports = router
